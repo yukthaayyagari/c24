ec9b93914c02ba29fceaae111d4ef37a24fbddf7
yukthaayyagari <89558629+yukthaayyagari@users.noreply.github.com>
2022-01-05 17:40:04 -0800

    Add files via upload

---
+++ Geologist-Template-main/BouncyBall.js
@@ -0,0 +1,33 @@
+class BouncyBall{
+	constructor(x,y,r)
+	{
+	
+		var options={
+		restitution:0.3,
+		friction:5,
+	 	density:1
+		}
+
+
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+		    var bouncyBallPos=this.body.position;		
+		    push()
+		    translate(bouncyBallPos.x, bouncyBallPos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			ellipse(0,0,this.r, this.r);
+			pop()
+	}
+
+}
